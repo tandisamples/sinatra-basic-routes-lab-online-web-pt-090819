35ce15c2ef259cad8b71e8db8f24a9ebb452f96b
Tandi Samples <tandisamples@gmail.com>
2019-12-08 21:06:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ class App < Sinatra::Base
   end
   
   get '/name' do
-    "My name is"
+    "My name is "
   end
   
   get '/hometown' do
-    "My hometown is"
+    "My hometown is "
   end
   
   get '/favorite-song' do
-    "My favorite song is"
+    "My favorite song is "
   end
   
   
